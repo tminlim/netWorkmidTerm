cc89eb6cd0d901fb270e71e934c103628cef4899
tminlim <tmin.jc@gmail.com>
2014-11-15 21:20:17 +0900

    echo server (no key input yet) & client complete

---
+++ client.c
@@ -0,0 +1,95 @@
+//
+// client.c
+// simple_client
+//
+// Created by Injae Lee on 2014. 10. 10.
+// Modified by Minsuk Lee,
+// Copyright (c) 2014. Injae Lee All rights reserved.
+// see LICENSE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/epoll.h>
+
+#define IP "127.0.0.1"
+#define PORT 3000
+#define BUF_SIZE 100
+#define EPOLL_SIZE 30
+
+int main()
+{
+    int ret = -1;
+    int clientSock, wsize;
+    struct sockaddr_in serverAddr;
+    char buf[BUF_SIZE];
+
+    struct epoll_event *epollEvents;
+    struct epoll_event event;
+    int epoolFd, eventCount, byteLength;    
+
+
+
+    if ((clientSock = socket(PF_INET, SOCK_STREAM, 0)) == -1) {
+        perror("socket");
+        goto leave;
+    }
+
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = inet_addr(IP);
+    serverAddr.sin_port = htons(PORT);
+
+    if ((ret = connect(clientSock,(struct sockaddr*)&serverAddr,
+                       sizeof(serverAddr)))) {
+        perror("connect");
+        goto error;
+    }
+
+    //set epoll
+    epoolFd = epoll_create(EPOLL_SIZE);
+    epollEvents = malloc(sizeof(struct epoll_event)*EPOLL_SIZE);
+
+    event.events = EPOLLIN;
+    event.data.fd = 0;
+    epoll_ctl(epoolFd, EPOLL_CTL_ADD, 0, &event);
+    event.data.fd = clientSock;
+    epoll_ctl(epoolFd, EPOLL_CTL_ADD, clientSock, &event);
+
+    while(1) {
+
+        eventCount = epoll_wait(epoolFd, epollEvents, EPOLL_SIZE, -1);
+        if(eventCount == -1) {
+            fprintf(stderr, "epoll_wait() error");
+            break;
+        }
+
+        for(int i = 0; i < eventCount; i++) { 
+            if(epollEvents[i].data.fd == 0) {
+                fgets(buf, BUF_SIZE, stdin);
+                if ((wsize = send(clientSock, buf, strlen(buf), 0)) <= 0) {
+                    perror("send");
+                    //fprintf(stderr, "error: %s", "send");
+                }
+            } else if(epollEvents[i].data.fd == clientSock) {
+                byteLength = read(clientSock, buf, BUF_SIZE - 1);//epollEvents[i].data.fd == clientSock
+                if(byteLength == 0) {
+                    epoll_ctl(epoolFd, EPOLL_CTL_DEL, clientSock, NULL);
+                    close(clientSock);
+                    goto leave;
+                } else {
+                    buf[byteLength] = '\0'; //convert string
+                    printf("%s\n",buf);
+                }          
+            }          
+        }
+    }
+
+error:
+    close(clientSock);
+leave:
+    return ret;
+}
+
--- server.c
+++ server.c
@@ -6,18 +6,36 @@
 #include <sys/socket.h>
 #include <sys/epoll.h>
 
+#ifndef SO_REUSEPORT
+#define SO_REUSEPORT 15
+#endif
+
+#define BUF_SIZE 100
+#define EPOLL_SIZE 30
+
 void error_handling(char *buf);
 
 int main (int argc, char* argv[]) {
-	int serverSocket;
-	struct sockaddr_in serverAddress;
+	int serverSocket, clientSocket;
+	struct sockaddr_in serverAddress, clientAddress;
+	socklen_t addrLength;
+
+	struct epoll_event *epollEvents;
+	struct epoll_event event;
+	int epoolFd, eventCount, byteLength;
+	char buf[BUF_SIZE];
+
+	if(argc != 2) {
+		printf("usage : %s <port>\n", argv[0]);
+		exit(1);
+	}
 
 	//open socket
 	serverSocket = socket(PF_INET, SOCK_STREAM, 0);
 	memset(&serverAddress, 0, sizeof(serverAddress));
 	serverAddress.sin_family = AF_INET;
 	serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
-	serverAddress.sin_port = htonl(atoi(argv[1]));
+	serverAddress.sin_port = htons(atoi(argv[1]));
 	
 	//bind
 	if(bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == -1)
@@ -26,9 +44,54 @@ int main (int argc, char* argv[]) {
 	//listen
 	if(listen(serverSocket, 5) == -1)
 		error_handling("listen error");
+
+	//set epoll
+	epoolFd = epoll_create(EPOLL_SIZE);
+	epollEvents = malloc(sizeof(struct epoll_event)*EPOLL_SIZE);
+
+	event.events = EPOLLIN;
+ 	event.data.fd = 0;
+     epoll_ctl(epoolFd, EPOLL_CTL_ADD, 0, &event);
+	event.data.fd = serverSocket;
+	epoll_ctl(epoolFd, EPOLL_CTL_ADD, serverSocket, &event);
+
+
+	while(1) {
+		eventCount = epoll_wait(epoolFd, epollEvents, EPOLL_SIZE, -1);
+		if(eventCount == -1) {
+			fprintf(stderr, "epoll_wait() error");
+			break;
+		}
 	//accept
-	//read n write
+	//read n write		
+
+		for(int i = 0; i < eventCount; i++) {
+			if(epollEvents[i].data.fd == serverSocket) {
+				addrLength = sizeof(clientAddress);
+				clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddress, &addrLength);
+				event.events = EPOLLIN;
+				event.data.fd = clientSocket;
+				epoll_ctl(epoolFd, EPOLL_CTL_ADD, clientSocket, &event);
+				printf("connect client %d\n", clientSocket);
+			} else {
+				byteLength = read(epollEvents[i].data.fd, buf, BUF_SIZE - 1);
+				if(byteLength == 0) {
+					epoll_ctl(epoolFd, EPOLL_CTL_DEL, epollEvents[i].data.fd, NULL);
+					close(epollEvents[i].data.fd);
+					//printf("close client! %d \n", epollEvents[i].data.fd );
+				} else {
+					buf[byteLength] = '\0'; //convert string
+					printf("%s\n",buf);
+					write(epollEvents[i].data.fd, buf, byteLength);
+				}
+			}
+		}
+	}
+
 	//close
+	close(serverSocket);
+	close(epoolFd);
+	return 0;
 }
 
 void error_handling(char *buf) {
